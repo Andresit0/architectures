cbf5761a19e91706e53616fb2dc02b9f0f201dde
Andrés Riofrío <rvandres001@gmail.com>
2026-07-31 09:44:32 -0500

    chore(platform): regenerate plugin registrants

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import connectivity_plus
 import flutter_secure_storage_darwin
-import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
