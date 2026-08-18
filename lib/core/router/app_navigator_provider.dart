86d999ae0b85291152f2f7a00f71964a0556cfbb
Andrés Riofrío <rvandres001@gmail.com>
2026-08-18 17:25:51 -0500

    refactor(router): throw SeamNotBoundException from appNavigatorProvider seam

--- lib/core/router/app_navigator_provider.dart
+++ lib/core/router/app_navigator_provider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:clean_architecture_sdd_harness/shared/exceptions/_exceptions.lib.dart';
 import 'package:clean_architecture_sdd_harness/shared/interfaces/_interfaces.lib.dart';
 
 final appNavigatorProvider = Provider<IAppNavigator>(
-  (ref) => throw UnimplementedError(
+  (ref) => throw SeamNotBoundException(
     'appNavigatorProvider must be overridden in the composition root '
     '(app/di/router/router_overrides.dart)',
   ),
