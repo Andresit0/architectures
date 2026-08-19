ea8d6e76964bfae3ebb8429f7701d81cab2472e1
Andresit0 <40771091+Andresit0@users.noreply.github.com>
2026-08-18 19:53:06 -0500

    refactor(clinical_history): adopt OnlineFirstRepository in repository (#89)
    ## Summary
    Refactors `ClinicalHistoryRepositoryImpl` to extend the
    `OnlineFirstRepository<ClinicalHistoryEntity>` template-method base.
    
    ## Why
    Removes duplicated online-first/cache logic in favor of the shared base
    (PR 4).
    
    ## Changes
    -
    `features/clinical_history/infrastructure/repositories/clinical_history_repository_impl.dart`:
    implement remoteLoader/localLoader/cacheWriter
    
    ## Testing
    - [x] flutter analyze
    - [x] dart format
    - [x] flutter test
    
    ## Stack
    | PR | Branch | Title |
    |----|--------|-------|
    | ... | ... | ... |
    | 8 | refactor/clinical-history-online-first |
    refactor(clinical_history): adopt OnlineFirstRepository |
    | ... | ... | ...

--- lib/features/clinical_history/infrastructure/repositories/clinical_history_repository_impl.dart
+++ lib/features/clinical_history/infrastructure/repositories/clinical_history_repository_impl.dart
@@ -1,60 +1,39 @@
+import 'package:clean_architecture_sdd_harness/core/repositories/online_first_repository.dart';
 import 'package:clean_architecture_sdd_harness/shared/error/_error.lib.dart';
-import 'package:clean_architecture_sdd_harness/shared/functions/online_first.dart';
-import 'package:clean_architecture_sdd_harness/shared/interfaces/_interfaces.lib.dart';
 import 'package:clean_architecture_sdd_harness/shared/models/_models.lib.dart';
 
 import '../../domain/datasources/i_clinical_history_local_datasource.dart';
 import '../../domain/datasources/i_clinical_history_remote_datasource.dart';
 import '../../domain/repositories/i_clinical_history_repository.dart';
 
-class ClinicalHistoryRepositoryImpl implements IClinicalHistoryRepository {
+class ClinicalHistoryRepositoryImpl
+    extends OnlineFirstRepository<ClinicalHistoryEntity>
+    implements IClinicalHistoryRepository {
   const ClinicalHistoryRepositoryImpl({
     required this._remoteDatasource,
     required this._localDatasource,
-    required this._logger,
-  });
+    required super.logger,
+  }) : super(logTag: 'clinical_history');
 
   final IClinicalHistoryRemoteDatasource _remoteDatasource;
   final IClinicalHistoryLocalDatasource _localDatasource;
-  final ILogger _logger;
 
   @override
-  Future<Result<List<ClinicalHistoryEntity>>> loadClinicalHistories() async {
-    final r = await fetchOrFallback(
-      remote: _remoteDatasource.loadRemote,
-      local: () async {
-        final data = await _localDatasource.loadLocal();
-        return data.isEmpty ? null : data;
-      },
-      onRemoteSuccess: (list) => _storeCache(list, 'load'),
-    );
-    _logger.info('[clinical_history] load origin=${r.origin.name}');
-    return r.result;
-  }
+  Future<List<ClinicalHistoryEntity>> remoteLoader() =>
+      _remoteDatasource.loadRemote();
 
   @override
-  Future<Result<List<ClinicalHistoryEntity>>> refreshClinicalHistories() async {
-    final result = await guard(() async {
-      final list = await _remoteDatasource.loadRemote();
-      await _storeCache(list, 'refresh');
-      return list;
-    });
-    _logger.info('[clinical_history] refresh origin=remote');
-    return result;
-  }
+  Future<List<ClinicalHistoryEntity>?> localLoader() =>
+      _localDatasource.loadLocal();
 
-  Future<void> _storeCache(
-    List<ClinicalHistoryEntity> list,
-    String context,
-  ) async {
-    try {
-      await _localDatasource.storeLocal(list);
-    } on Exception catch (e, st) {
-      _logger.error(
-        '[clinical_history] cache write failed ($context)',
-        technicalMessage: e.toString(),
-        stackTrace: st,
-      );
-    }
-  }
+  @override
+  Future<void> cacheWriter(List<ClinicalHistoryEntity> data) =>
+      _localDatasource.storeLocal(data);
+
+  @override
+  Future<Result<List<ClinicalHistoryEntity>>> loadClinicalHistories() => load();
+
+  @override
+  Future<Result<List<ClinicalHistoryEntity>>> refreshClinicalHistories() =>
+      refresh();
 }
